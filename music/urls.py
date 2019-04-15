2034c9d72083f9a4de3370b1a06f610b9cf73bfd
Shihab Uddin Piash <Piash222@gmail.com>
2019-04-15 12:27:27 +0600

    function based view for album and album details

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
+    # /music/
     path('', views.index, name="index"),
+
+    # /music/id/
+    path("<int:album_id>/", views.detail, name="detail"),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Album
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums': all_albums
+    }
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
